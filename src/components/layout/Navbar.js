4b927ff66d445036a1e5110466899b25a9dc3a05
Gati <ezequielgatica@gmail.com>
2020-10-11 14:25:47 -0300

    borrar archivos innecesarios y estructura de archivos

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div>
+            Hola soy una navbar
+        </div>
+    )
+}
+
+export default Navbar
---
+++ src/components/pages/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Home extends Component {
+    render() {
+        return (
+            <div>
+                Hola soy una home!
+            </div>
+        )
+    }
+}
+
+export default Home
