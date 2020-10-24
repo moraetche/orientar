3955bedc89a46dffe2afd4755eeec3636e43e8a8
Eze Gatica <ezequielgatica@gmail.com>
2020-10-24 22:22:26 +0000

    header de la home :D

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,30 +1,30 @@
 import React, { Component } from 'react'
 import '../css/layout.css'
-import {NavLink} from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 export class Navbar extends Component {
-    
-    render() {
-            return(
-    <>
-      <ul id="dropdown1" class="dropdown-content">
-        <li><NavLink to="#!">one</NavLink></li>
-        <li><NavLink to="#!">two</NavLink></li>
-        <li class="divider"></li>
-        <li><NavLink to="#!">three</NavLink></li>
-      </ul>
 
-      <nav class="navbar">
-        <div class="nav-wrapper">
-          <NavLink to="/" class="brand-logo">Orientar</NavLink>
-          <ul class="right hide-on-med-and-down">
-            <li><NavLink class="dropdown-trigger dropdown-areas" to="#areas" data-target="dropdown1">Áreas<i class="material-icons right">arrow_drop_down</i></NavLink></li>
-          </ul>
-        </div>
-      </nav>
-    </>
-      )
-    }
+  render() {
+    return (
+      <>
+        <ul id="dropdown1" className="dropdown-content">
+          <li><NavLink to="#!">one</NavLink></li>
+          <li><NavLink to="#!">two</NavLink></li>
+          <li className="divider"></li>
+          <li><NavLink to="#!">three</NavLink></li>
+        </ul>
+
+        <nav className="navbar">
+          <div className="nav-wrapper">
+            <NavLink to="/" className="brand-logo">Orientar</NavLink>
+            <ul className="right hide-on-med-and-down">
+              <li><NavLink className="dropdown-trigger dropdown-areas" to="#areas" data-target="dropdown1">Áreas<i className="material-icons right">arrow_drop_down</i></NavLink></li>
+            </ul>
+          </div>
+        </nav>
+      </>
+    )
+  }
 }
 
 export default Navbar
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react'
-
+import homeimg from "../assets/homeimg.jpg"
+import '../css/home.css'
+import M from 'materialize-css'
 export class Home extends Component {
+  componentDidMount=()=>{
+    var options
+    var elems = document.querySelectorAll('.slider');
+    var instances = M.Slider.init(elems, options);
+    options = instances;
+  }
     render() {
         return (
-            <div>
-                Hola soy una home!
-                buenas!
-            </div>
+          
+  <div class="slider slider-home">
+    <ul class="slides">
+      <li>
+        <img src={require("../assets/homeimg.jpg")} alt="imagen home" className="img-home"/>
+      
+        <div class="caption center-align">
+          <h3 className= "banner">A un paso de tu futuro, <br/>a un click de la mejor elección.</h3>
+           {/* <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>*/}
+        </div>
+      </li>
+    </ul>
+  </div>
         )
     }
 }
