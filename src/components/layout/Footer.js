0c3049e21ac84c578ce1a04921128d501d496ff5
moraetchezahar <moraetche@gmail.com>
2020-10-30 02:26:08 +0000

    recupere el footer uwu!!! :D

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import '../css/layout.css'
+export class Footer extends Component {
+    render() {
+        return (
+            <div className="footer">
+            </div>
+        )
+    }
+}
+
+export default Footer 
