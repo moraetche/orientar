0de249e8918423c106a802fb23ea2f1332acc6ff
Mora <moraetche@gmail.com>
2020-11-08 20:13:29 -0300

    paginas de areas con el diseño, falta cambiar el state de cada uno

--- src/components/pages/Carrera.js
+++ src/components/pages/Carrera.js
@@ -2,21 +2,31 @@
 import React, { Component } from 'react'
 
 export class Carrera extends Component {
-  state={
+  state = {
+
+  }
+  chequearInformacion = () => {
+    switch (this.props.match.params.carrera) {
+      case 'value':
+        
+        break;
     
-  }
-  componentDidMount = () =>{
-    console.log("PROPS:", this.props)     
-  }
-    render() {
-        return (
-            <div className="center">
-            <h1> ARCHIVO CARRERA.JS </h1>
-            Area: {this.props.match.params.area} <br/>
-            Carrera: {this.props.match.params.carrera}
-            </div>
-        )
+      default:
+        break;
     }
+  }
+  componentDidMount = () => {
+    console.log("PROPS:", this.props)
+  }
+  render() {
+    return (
+      <div className="center">
+        <h1> ARCHIVO CARRERA.JS </h1>
+            Area: {this.props.match.params.area} <br />
+            Carrera: {this.props.match.params.carrera}
+      </div>
+    )
+  }
 }
 
 export default Carrera
--- src/components/pages/areas/Artes.js
+++ src/components/pages/areas/Artes.js
@@ -2,18 +2,36 @@ import React, { Component } from 'react'
 import '../../css/home.css'
 import '../../css/area.css'
 import M from 'materialize-css'
-//import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 export class Ingenieria extends Component {
     state = {
         titulo: 'Ingenieria',
+        id: 'ingenieria',
         imagen: require("../../assets/ingenieriaimg.jpg"),
         carrera1: {
+            id: 'agronoma',
             nombre: 'Ingenieria agrónoma',
             imagen: require('../../assets/ingenieria/agronomaimg.jpg'),
             descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint necessitatibus deleniti nihil tempore, nisi odio veniam voluptates facere? Nesciunt dolorum consectetur nihil molestiae iste sed velit. Culpa, molestias! Culpa, vitae.'
-        }
+        },
+        carrera2: {
+            id: 'industrial',
+            nombre: 'Ingenieria industrial',
+            imagen: require('../../assets/ingenieria/industrialimg.jpg'),
+            descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint necessitatibus deleniti nihil tempore, nisi odio veniam voluptates facere? Nesciunt dolorum consectetur nihil molestiae iste sed velit. Culpa, molestias! Culpa, vitae.'
+        },
     }
     componentDidMount = () => {
+        // let id = this.props.match.path
+        // let fixed = ""
+        // for (let i = 0; i < id.length; i++) {
+        //     const element = id[i];
+        //     if (i !== 0){
+        //         fixed = fixed + element
+        //     }
+        // }
+        // console.log(fixed);
+        // console.log(this.props.match.path);
         var options
         var elems = document.querySelectorAll('.slider');
         var instances = M.Slider.init(elems, options);
@@ -25,8 +43,7 @@ export class Ingenieria extends Component {
                 <div class="slider slider-home sl-home">
                     <ul class="slides slider-home">
                         <li>
-                            <img src={this.state.imagen} alt="imagen Artes" className="img-home" />
-
+                            <img src={this.state.imagen} alt="imagen Ingenieria" className="img-home" />
                             <div class="caption left-align">
                                 <h3 className="banner">{this.state.titulo}</h3>
                                 <h5 class="descripcion-area">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat.
@@ -37,32 +54,44 @@ export class Ingenieria extends Component {
                 </div>
                 <div className="areas-carreras container">
 
+                    <br />
                     <div className="row ">
                         <div className="col s6">
-                            <h3 className="left-align">
-                                {this.state.carrera1.nombre}
-                            </h3>
-                            <h5>{this.state.carrera1.descripcion}</h5>
+                            <Link to={this.props.match.path + "/" + this.state.carrera1.id}>
+                                <h3 className="left-align titulo-carrera">
+                                    {this.state.carrera1.nombre}
+                                </h3>
+                            </Link>
+                            <h5 className="descripcion-carrera">{this.state.carrera1.descripcion}</h5>
                         </div>
                         <div className="col s6">
                             <div className="left">
-                                <img src={this.state.carrera1.imagen} alt="" className="responsive-img imagen-area" />
+                                <Link to={this.props.match.path + "/" + this.state.carrera1.id}>
+                                    <img src={this.state.carrera1.imagen} alt="" className="responsive-img imagen-area z-depth-4 hoverable" />
+                                </Link>
+
                             </div>
                         </div>
                     </div>
+                    <br />
                     <div className="row">
                         <div className="col s6">
                             <div className="left">
-                                <img src={this.state.carrera1.imagen} alt="" className="responsive-img imagen-area" />
+                                <Link to={this.props.match.path + "/" + this.state.carrera2.id}>
+                                    <img src={this.state.carrera2.imagen} alt="" className="responsive-img imagen-area z-depth-4 hoverable" />
+                                </Link>
                             </div>
                         </div>
                         <div className="col s6">
-                            <h3 className="left-align">
-                                {this.state.carrera1.nombre}
-                            </h3>
-                            <h5>{this.state.carrera1.descripcion}</h5>
+                            <Link to={this.props.match.path + "/" + this.state.carrera2.id}>
+                                <h3 className="left-align titulo-carrera">
+                                    {this.state.carrera2.nombre}
+                                </h3>
+                            </Link>
+                            <h5 className="descripcion-carrera">{this.state.carrera2.descripcion}</h5>
                         </div>
                     </div>
+                    <br />
                 </div>
 
             </div>
