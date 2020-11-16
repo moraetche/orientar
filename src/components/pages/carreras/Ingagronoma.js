fc1fc3b92fc7f5188a6ffd7bf3e1e182aa4a570a
Sancho6446 <70781480+Sancho6446@users.noreply.github.com>
2020-11-16 02:07:31 -0300

    harcode faltan imagenes

--- src/components/pages/carreras/Ingagronoma.js
+++ src/components/pages/carreras/Ingagronoma.js
@@ -21,13 +21,14 @@ export class Ingagronoma extends Component {
             <br></br>
 						<h2>Ingeniería agrónoma</h2>
 						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat nam cupiditate delectus labore ratione
-							eaque laudantium, in nisi est
+						El ingeniero agrónomo es el profesional de la ingeniería y la tecnología encargado de manejar los recursos naturales de manera racional para desarrollar el sector agropecuario, el medio rural y la industria agroalimentaria. Ellos hacen posible que los agricultores y los ganaderos trabajen de un modo más eficiente
 						</p>
-						<li className= 'lista-de-info'>Item 1</li>
-						<li className= 'lista-de-info'>Item 2</li>
-						<li className= 'lista-de-info'>Item 3</li>
-						<li className= 'lista-de-info'>Item 4</li>
+						<li className= 'lista-de-info'>Carga horaria total: 3.752</li>
+						<li className= 'lista-de-info'>Duración estimada en años: 5,5</li>
+						<li className= 'lista-de-info'>Título intermedio: Bachiller Universitario en Agronomía</li>
+						<li className= 'lista-de-info'>Título final: Ingeniero Agrónomo</li>
+						<li className= 'lista-de-info'>Facultad de Agronomía: Av. San Martín 4453, C1417DSE, Ciudad Autónoma de Buenos Aires.</li>
+
 					</div>
 					<div className='col m6'>
 						<img className='imgcarreras' src='https://picsum.photos/300' alt='' />
@@ -39,33 +40,30 @@ export class Ingagronoma extends Component {
 					<div className='punteo'>
 						<div className='circle'></div>
 						<p>
-							Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit quas ipsam quo architecto libero
-							temporibus? Nihil vitae ex accusantium ab rerum magnam, aut, hic assumenda, modi possimus nam fuga in?
+						No encontramos cursante.
 						</p>
 					</div>
           <h4 className= 'opiniones'>¿Qué te hubiera gustado saber antes de empezar/elegir la carrera?/Que le dirías a alguien antes de elegir la carrera?</h4>
 					<div className='punteo'>
 						<div className='circle'></div>
 						<p>
-							Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit quas ipsam quo architecto libero
-							temporibus? Nihil vitae ex accusantium ab rerum magnam, aut, hic assumenda, modi possimus nam fuga in?
+							No encontramos cursante.
 						</p>
 					</div>
           <h4 className= 'opiniones'>Consejo para los que empiezan</h4>
 					<div className='punteo'>
 						<div className='circle'></div>
 						<p>
-							Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit quas ipsam quo architecto libero
-							temporibus? Nihil vitae ex accusantium ab rerum magnam, aut, hic assumenda, modi possimus nam fuga in?
+						No encontramos cursante.
 						</p>
 					</div>
           <h6> Donde contactar: </h6>
-            <li><a href="http://www.uba.ar/download/academicos/carreras/abogacia.pdf">Link plan de estudios</a></li>
-						<li className= 'contactos'>Item 2</li>
-            <li><a href="http://www.uba.ar/download/academicos/carreras/abogacia.pdf">Link plan de estudios</a></li>
-            <li><a href="http://www.uba.ar/download/academicos/carreras/abogacia.pdf">Link plan de estudios</a></li>
-            <li><a href="http://www.uba.ar/download/academicos/carreras/abogacia.pdf">Link plan de estudios</a></li>
-            <li><a href="http://www.uba.ar/download/academicos/carreras/abogacia.pdf">Link plan de estudios</a></li>
+		                <li className= 'contactos'><a href="https://www.agro.uba.ar/sites/default/files/carreras/agro_plan_2017_julio_19.pdf">Link plan de estudios</a></li>
+						
+						<li className= 'contactos'><a href="https://www.agro.uba.ar/alumnos/comoingreso">Link Inscripcion</a></li>
+						<li className= 'contactos'><a href="https://www.agro.uba.ar/carreras/agronomia">Link Página carrera</a></li>
+						<li className= 'contactos'>Mail consultas: diragron@agro.uba.ar</li>
+						<li className= 'contactos'>Telefono consultas: (+54) 11 5287-0032</li>
 				</div>
 			</div>
 		);
--- src/components/pages/carreras/Ingindustrial.js
+++ src/components/pages/carreras/Ingindustrial.js
@@ -21,13 +21,14 @@ export class Ingindustrial extends Component {
             <br></br>
 						<h2>Ingeniería industrial</h2>
 						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat nam cupiditate delectus labore ratione
-							eaque laudantium, in nisi est
+						La Ingeniería Industrial es por definición la rama de las ingenierías encargada del análisis, interpretación, comprensión, diseño, programación y control de sistemas productivos y logísticos con miras a gestionar, implementar y establecer estrategias de optimización con el objetivo de lograr el máximo rendimiento de los procesos de creación de bienes y/o la prestación de servicios.
 						</p>
-						<li className= 'lista-de-info'>Item 1</li>
-						<li className= 'lista-de-info'>Item 2</li>
-						<li className= 'lista-de-info'>Item 3</li>
-						<li className= 'lista-de-info'>Item 4</li>
+						<li className= 'lista-de-info'>Carga horaria total: 4528</li>
+						<li className= 'lista-de-info'>Duración estimada en años: 6</li>
+						<li className= 'lista-de-info'>Título final: Ingeniero/a Industrial</li>
+						<li className= 'lista-de-info'>Facultad de Ingeniería:Paseo Colón 850, C1063ACV, Ciudad Autónoma de Buenos Aires.</li>
+						<li className= 'lista-de-info'>Facultad de Ingeniería:Intendente Güiraldes 2160, Pabellón III, C1428EGA, Ciudad Autónoma de Buenos Aires.</li>
+						<li className= 'lista-de-info'>Facultad de Ingeniería:Av. Las Heras 2214, C1127AAR, Ciudad Autónoma de Buenos Aires.</li>
 					</div>
 					<div className='col m6'>
 						<img className='imgcarreras' src='https://picsum.photos/300' alt='' />
@@ -39,33 +40,36 @@ export class Ingindustrial extends Component {
 					<div className='punteo'>
 						<div className='circle'></div>
 						<p>
-							Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit quas ipsam quo architecto libero
-							temporibus? Nihil vitae ex accusantium ab rerum magnam, aut, hic assumenda, modi possimus nam fuga in?
+						Matemática, Física, Química
 						</p>
 					</div>
           <h4 className= 'opiniones'>¿Qué te hubiera gustado saber antes de empezar/elegir la carrera?/Que le dirías a alguien antes de elegir la carrera?</h4>
 					<div className='punteo'>
 						<div className='circle'></div>
 						<p>
-							Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit quas ipsam quo architecto libero
-							temporibus? Nihil vitae ex accusantium ab rerum magnam, aut, hic assumenda, modi possimus nam fuga in?
+						Preparate para aprender de todas las áreas, si sos una persona que quiere entender de todo un poco, estás en el lugar correcto. Ingeniería industrial te va a abrir las puertas para más adelante poder especializarte y/o adentrarte en (casi) lo que quieras. El ingeniero o la ingeniera industrial está diseñad@ para tener puestos altos en rango, de liderazgo, y por lo tanto debe entender todo lo que ocurre en cada sector de su negocio o empresa.
+Sin embargo, estás eligiendo una carrera de ingeniería, no es nada en comparación con lo que viviste en la secundaria (y tampoco el CBC, si es que finalizaste esa etapa). Para poder llevar a cabo una carrera de este estilo, vas a tener que estar segur@ que este es tu lugar, que esto es lo que te gusta, y que vas a estar dispuest@ a dar el esfuerzo extra cuando tod@s a tu alrededor pasan su carrera sin transpirar. No es tu caso, bienvenid@ a ingeniería.
+
 						</p>
 					</div>
           <h4 className= 'opiniones'>Consejo para los que empiezan</h4>
 					<div className='punteo'>
 						<div className='circle'></div>
 						<p>
-							Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit quas ipsam quo architecto libero
-							temporibus? Nihil vitae ex accusantium ab rerum magnam, aut, hic assumenda, modi possimus nam fuga in?
+						Movete, no te dejes estar. Es casi indispensable que consigas contactos que ya están más avanzad@s en la carrera, no tengas miedo de preguntarles todo, ell@s tuvieron que hacer lo mismo. Encontrar un grupo de gente que tenga tu ritmo de estudio y tu velocidad te va a ayudar a pasar la carrera mucho más fácil.
+Es muy importante que no te compares todo el tiempo, siempre va a haber gente que "ya tiene todo estudiado/sabido", que te sirva como motivación para dar el esfuerzo extra.
+La primera parte de la carrera es LEJOS la más difícil, ayudate de apuntes, de resueltos (¡chequeá que estos sean correctos!) y de lo que tengas disponible. Organizate bien, y si no sos una persona ordenada, amigate de alguien que lo sea. PRIORIZAR CATEDRA ANTES QUE HORARIOS siempre que sea posible.
+Estudiar todos los días no es una mala idea, estar al día con Ingeniería es muy difícil, pero te asegurás ser del 5% que no se queda atrás ni deja la carrera en los primeros años.
+
 						</p>
 					</div>
           <h6> Donde contactar: </h6>
-            <li><a href="http://www.uba.ar/download/academicos/carreras/abogacia.pdf">Link plan de estudios</a></li>
-						<li className= 'contactos'>Item 2</li>
-            <li><a href="http://www.uba.ar/download/academicos/carreras/abogacia.pdf">Link plan de estudios</a></li>
-            <li><a href="http://www.uba.ar/download/academicos/carreras/abogacia.pdf">Link plan de estudios</a></li>
-            <li><a href="http://www.uba.ar/download/academicos/carreras/abogacia.pdf">Link plan de estudios</a></li>
-            <li><a href="http://www.uba.ar/download/academicos/carreras/abogacia.pdf">Link plan de estudios</a></li>
+		                <li className= 'contactos'><a href="http://www.fi.uba.ar/sites/default/files/Ingenieria%20Industrial%202011%20Modificacion%202018%20actualizacion%202019-.pdf">Link plan de estudios</a></li>
+						
+						<li className= 'contactos'><a href="http://www.fi.uba.ar/es/node/744">Link Inscripcion</a></li>
+						<li className= 'contactos'><a href="http://www.fi.uba.ar/es/node/216">Link Página carrera</a></li>
+						<li className= 'contactos'>Mail consultas: academica@fi.uba.ar</li>
+						<li className= 'contactos'>Telefono consultas: (+54) 11 5285-0400/01</li>
 				</div>
 			</div>
 		);
